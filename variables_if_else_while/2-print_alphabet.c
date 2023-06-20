ff04c489d78e729f5e572871bb2b1b11c31a72df
gabesantiago <gasantiago98@gmail.com>
2023-06-20 12:29:32 +0000

    You can target every value in an array with |for (i=0; i < *amount of values in the array* ; i++)| by declaring i as an integer type variable and then placing i in the brackets of the array name like so: alp[i]

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,17 @@
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	char alp[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char alp[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
-	}	
+	}
 	putchar('\n');
 
 	return (0);
