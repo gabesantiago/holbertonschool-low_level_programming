b5f1f54a61a456b72096bbeccf303d17cb7b1de0
gabesantiago <gasantiago98@gmail.com>
2023-06-20 14:48:14 +0000

    Printing all single digit numbers in base 10 with a for loop

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int base_10;
+
+	for (base_10 = 0; base_10 <= 10; base_10++)
+	{
+		putchar(base_10);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
