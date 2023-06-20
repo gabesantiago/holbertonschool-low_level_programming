c8e02cf2dc590964f82bd4d138ea608690fadd0a
gabesantiago <gasantiago98@gmail.com>
2023-06-20 13:34:01 +0000

    Continue lets you jump/skip over a value in the array and continue with the function

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a newline EXCEPT q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alpha == e, q)
+		{
+			continue;
+		}
+		putchar(alpha[i]);
+	}
+
+	putchar("\n");
+
+	return (0);
+
+}
