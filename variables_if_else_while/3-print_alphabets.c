58dc7084556156e5d57678d4ce8e3af9684b069e
gabesantiago <gasantiago98@gmail.com>
2023-06-20 12:56:22 +0000

    Same thing as problem 2 but with both lower and uppercase

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	char alp[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 	'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 	'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 	int i;
 
 	for (i = 0; i < 52; i++)
-        {
+	{
 	putchar(alp[i]);
 	}
 	putchar('\n');
